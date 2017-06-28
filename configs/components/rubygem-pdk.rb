25e376f3b8bb6eca4caed6040c4d489e958e3d2c
Jesse Scott <jesse@puppetlabs.com>
2017-06-28 15:43:27 -0700

    (SDK-273) Add basic PowerShell console shortcut

--- configs/components/rubygem-pdk.rb
+++ configs/components/rubygem-pdk.rb
@@ -19,10 +19,12 @@ component "rubygem-pdk" do |pkg, settings, platform|
   if platform.is_windows?
     pkg.add_source("file://resources/files/windows/environment.bat", sum: "e7453da7fc1e5ad98125d099d4e11fa4")
     pkg.add_source("file://resources/files/windows/pdk_shell.bat", sum: "051a4c252e5239b9bc49a47bc92e21d9")
+    pkg.add_source("file://resources/files/windows/pdk_shell.ps1", sum: "da73feba1043e2de87475f8eb02027c9")
     pkg.add_source("file://resources/files/windows/pdk.bat", sum: "f4364047a4d851650adb350b03aafd4b")
 
     pkg.install_file "../environment.bat", "#{settings[:main_bin]}/environment.bat"
     pkg.install_file "../pdk_shell.bat", "#{settings[:main_bin]}/pdk_shell.bat"
+    pkg.install_file "../pdk_shell.ps1", "#{settings[:main_bin]}/pdk_shell.ps1"
     pkg.install_file "../pdk.bat", "#{settings[:main_bin]}/pdk.bat"
   else
     pkg.link "#{settings[:ruby_bindir]}/pdk", "#{settings[:link_bindir]}/pdk"

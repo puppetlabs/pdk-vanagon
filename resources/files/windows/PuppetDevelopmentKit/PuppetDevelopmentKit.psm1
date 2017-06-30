b7828fa42947606b1236dc07e7455aea4a60f4d9
James Pogran <james.pogran@puppetlabs.com>
2017-06-30 12:49:50 -0400

    (SDK-301) Add PDK PowerShell Module
    This commit adds a PowerShell module that replaces the batch scripts previously used to start the PDK on windows

---
+++ resources/files/windows/PuppetDevelopmentKit/PuppetDevelopmentKit.psm1
@@ -0,0 +1,11 @@
+$env:DEVKIT_BASEDIR = (Get-ItemProperty -Path "HKLM:\Software\Puppet Labs\DevelopmentKit").RememberedInstallDir64
+$env:RUBY_DIR       = "$($env:DEVKIT_BASEDIR)\private\ruby\2.1.9"
+$env:PATH           = "$($env:DEVKIT_BASEDIR)\bin;%PATH%"
+$env:SSL_CERT_FILE  = "$($env:DEVKIT_BASEDIR)\ssl\cert.pem"
+$env:SSL_CERT_DIR   = "$($env:DEVKIT_BASEDIR)\ssl\certs"
+
+function pdk{
+  &$env:RUBY_DIR\bin\ruby -S -- $env:RUBY_DIR\bin\pdk $args
+}
+
+Export-ModuleMember -Function pdk -Variable *
--- resources/files/windows/pdk.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-SETLOCAL
-
-call "%~dp0environment.bat" %0 %*
-
-%DEVKIT_BASEDIR%\private\tools\bin\ansicon.exe %RUBY_DIR%\bin\ruby -S -- %RUBY_DIR%\bin\pdk %*

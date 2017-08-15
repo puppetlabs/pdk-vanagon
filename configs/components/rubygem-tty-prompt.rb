5203a0ab97a5803aee0b745a61ee400e8379e293
Austin Blatt <austinblatt@gmail.com>
2017-08-15 15:36:25 -0700

    (MAINT) Bump tty-prompt version

--- configs/components/rubygem-tty-prompt.rb
+++ configs/components/rubygem-tty-prompt.rb
@@ -1,6 +1,6 @@
 component "rubygem-tty-prompt" do |pkg, settings, platform|
-  pkg.version "0.13.0"
-  pkg.md5sum "b44d04e0e1e9ec0e701d32949f828c6e"
+  pkg.version "0.13.1"
+  pkg.md5sum "033bca393ff6b7d67dd560cadf2be8c9"
   pkg.url "http://buildsources.delivery.puppetlabs.net/tty-prompt-#{pkg.get_version}.gem"
 
   pkg.build_requires "ruby-#{settings[:ruby_version]}"

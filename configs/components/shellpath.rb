40ff8697cfc1fc3f8a6c160a2e11e7700d01af15
james-stocks <james.stocks@puppetlabs.com>
2017-08-29 15:57:58 +0000

    (PDK-446) Move pdk to /opt/puppetlabs/pdk/bin and symlink on Debian
    Move pdk from /opt/puppetlabs/bin to /opt/puppetlabs/pdk/bin
    Fix adding pdk to PATH on debian|ubuntu

--- configs/components/shellpath.rb
+++ configs/components/shellpath.rb
@@ -1,9 +1,11 @@
 component "shellpath" do |pkg, settings, platform|
   if platform.is_macos?
-    pkg.add_source 'file://resources/files/paths.d/50-pdk', sum: '077ceb5e2f71cf733190a61d2fd221fb'
+    pkg.add_source 'file://resources/files/paths.d/50-pdk', sum: '4abf75aebbbfbbefc4fe0173c57ed0b2'
     pkg.install_file('./50-pdk', '/etc/paths.d/50-pdk')
+  elsif platform.is_deb?
+    pkg.link File.join(settings[:prefix], 'bin', 'pdk'), File.join(settings[:main_bin], 'pdk')
   elsif platform.is_linux?
-    pkg.add_source "file://resources/files/profile.d/pdk.sh", sum: "b6a51cfe9c7d8e435be4f7866a847d69"
+    pkg.add_source "file://resources/files/profile.d/pdk.sh", sum: "432bafccde488f7624d6c384ff8c15b6"
     pkg.install_file('./pdk.sh', '/etc/profile.d/pdk.sh')
   end
 end
--- resources/files/profile.d/pdk.sh
+++ resources/files/profile.d/pdk.sh
@@ -1,5 +1,5 @@
 # Add /opt/puppetlabs/bin to the path for sh compatible users
 
-if ! echo "$PATH" | grep -q /opt/puppetlabs/bin ; then
-  export PATH=$PATH:/opt/puppetlabs/bin
+if ! echo "$PATH" | grep -q /opt/puppetlabs/pdk/bin ; then
+  export PATH=$PATH:/opt/puppetlabs/pdk/bin
 fi

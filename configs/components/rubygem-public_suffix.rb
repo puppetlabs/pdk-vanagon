41d6bacc7224f4dd1647698686b403ae037eed0f
Bryan Jen <bryan.jen@gmail.com>
2017-10-04 14:56:16 -0700

    (PDK-572) Add json-schema and deps to the project

---
+++ configs/components/rubygem-public_suffix.rb
@@ -0,0 +1,15 @@
+component "rubygem-public_suffix" do |pkg, settings, platform|
+  pkg.version "3.0.0"
+  pkg.md5sum "787a298cfb1e4d9f39aa43a415d53e6f"
+  pkg.url "http://buildsources.delivery.puppetlabs.net/public_suffix-#{pkg.get_version}.gem"
+
+  pkg.build_requires "ruby-#{settings[:ruby_version]}"
+
+  if platform.is_windows?
+    pkg.environment "PATH", settings[:gem_path_env]
+  end
+
+  pkg.install do
+    "#{settings[:gem_install]} public_suffix-#{pkg.get_version}.gem"
+  end
+end

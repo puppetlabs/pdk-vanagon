3b8115da2b3b78078917a2709e5043dc30bb2276
David Schmitt <david.schmitt@puppet.com>
2017-10-18 15:49:54 +0100

    (PDK-484) Copy new curl version from puppet-agent
    Originally from https://github.com/puppetlabs/puppet-agent/commit/8cc295fa39ff6fa53d3045c5f1ecd8ea312e1db7
    
    This preserves our "cleanup" installation step.

--- configs/components/curl.rb
+++ configs/components/curl.rb
@@ -1,15 +1,23 @@
 component 'curl' do |pkg, settings, platform|
-  pkg.version '7.51.0'
-  pkg.md5sum '490e19a8ccd1f4a244b50338a0eb9456'
+  pkg.version '7.56.0'
+  pkg.md5sum '65351b9df687ed539852ae7b9464006c'
   pkg.url "https://curl.haxx.se/download/curl-#{pkg.get_version}.tar.gz"
 
   pkg.build_requires "openssl"
   pkg.build_requires "puppet-ca-bundle"
 
+  if platform.is_cross_compiled_linux?
+    pkg.build_requires 'runtime'
+    pkg.environment "PATH" => "/opt/pl-build-tools/bin:$$PATH:#{settings[:bindir]}"
+    pkg.environment "PKG_CONFIG_PATH" => "/opt/puppetlabs/puppet/lib/pkgconfig"
+    pkg.environment "PATH" => "/opt/pl-build-tools/bin:$$PATH"
+  end
+
   if platform.is_windows?
     pkg.build_requires "runtime"
-    pkg.environment "PATH", "$(shell cygpath -u #{settings[:gcc_bindir]}):$$PATH"
-    pkg.environment "CYGWIN", settings[:cygwin]
+
+    pkg.environment "PATH" => "$$(cygpath -u #{settings[:gcc_bindir]}):$$PATH"
+    pkg.environment "CYGWIN" => settings[:cygwin]
   end
 
   pkg.configure do

02b8076740eb2184006a7a68f1fdb3fb28899f86
Morgan Rhodes <morgan@puppet.com>
2017-11-30 14:55:47 -0800

    (maint) Add zlib dependency to nokogiri dependency
    This was previously picked up by the build dependency of ruby, but since
    that has moved to the runtime we need to make sure we're including those
    dependencies for nokogiri.

--- configs/components/rubygem-nokogiri.rb
+++ configs/components/rubygem-nokogiri.rb
@@ -15,6 +15,7 @@ component 'rubygem-nokogiri' do |pkg, settings, platform|
     pkg.install do
       ["#{settings[:gem_install]} #{gemname}-#{pkg.get_version}-x64-mingw32.gem"]
     end
+    pkg.build_requires "pl-zlib-#{platform.architecture}"
   else
     pkg.build_requires 'cmake'
 
@@ -24,6 +25,14 @@ component 'rubygem-nokogiri' do |pkg, settings, platform|
     else
       # debian
       pkg.build_requires 'pkg-config'
+    end
+
+    if platform.is_deb?
+      pkg.build_requires "zlib1g-dev"
+    elsif platform.is_aix?
+      pkg.build_requires "http://osmirror.delivery.puppetlabs.net/AIX_MIRROR/zlib-devel-1.2.3-4.aix5.2.ppc.rpm"
+    elsif platform.is_rpm?
+      pkg.build_requires "zlib-devel"
     end
 
     pkg.install do

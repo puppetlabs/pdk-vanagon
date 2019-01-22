10cb74c6cc3d6291c5cc96979cbe3a4578e2eab3
Tim Sharpe <tim@bombasticmonkey.com>
2019-01-22 11:43:49 +1100

    (PDK-632) Ensure clean execution env on Windows

--- resources/files/windows/PuppetDevelopmentKit/PuppetDevelopmentKit.psm1
+++ resources/files/windows/PuppetDevelopmentKit/PuppetDevelopmentKit.psm1
@@ -8,6 +8,25 @@ $env:SSL_CERT_FILE  = "$($env:DEVKIT_BASEDIR)\ssl\cert.pem"
 $env:SSL_CERT_DIR   = "$($env:DEVKIT_BASEDIR)\ssl\certs"
 
 function pdk {
+  $envvars = @(
+    'Env:\RUBYLIB'
+    'Env:\RUBYLIB_PREFIX'
+    'Env:\RUBYOPT'
+    'Env:\RUBYPATH'
+    'Env:\RUBYSHELL'
+    'Env:\DLN_LIBRARY_PATH'
+    'Env:\LD_PRELOAD'
+    'Env:\LD_LIBRARY_PATH'
+    'Env:\GEM_HOME'
+    'Env:\GEM_PATH'
+  )
+
+  $envvars | ForEach {
+    if (Test-Path $_) {
+      Remove-Item -Path $_ -Force
+    }
+  }
+
   if ($env:ConEmuANSI -eq 'ON') {
     &$env:RUBY_DIR\bin\ruby -S -- $env:RUBY_DIR\bin\pdk $args
   } else {

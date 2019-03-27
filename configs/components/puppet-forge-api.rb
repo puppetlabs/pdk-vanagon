a517a308fe3b85375814ad97baec7153a2059b3c
Jesse Scott <jesse@puppetlabs.com>
2019-03-27 11:17:06 -0700

    (PDK-1302) (PDK-1303) (PDK-1304) Ensure we are using latest rubygems

--- configs/components/puppet-forge-api.rb
+++ configs/components/puppet-forge-api.rb
@@ -62,22 +62,35 @@ component "puppet-forge-api" do |pkg, settings, platform|
       ].join(' ')
     end
 
-    build_commands = []
+    rubygems_update = lambda do |ruby_version, ruby_api|
+      rubygems_update_commands = []
 
-    settings[:additional_rubies]&.each do |rubyver, local_settings|
       # Make backups of the gem and bundler wrapper batch files...
-      build_commands << "cp #{gem_bins[local_settings[:ruby_api]]} #{gem_bins[local_settings[:ruby_api]]}.bak" if platform.is_windows?
-      build_commands << "cp #{bundle_bins[local_settings[:ruby_api]]} #{bundle_bins[local_settings[:ruby_api]]}.bak" if platform.is_windows?
+      rubygems_update_commands << "cp #{gem_bins[ruby_api]} #{gem_bins[ruby_api]}.bak" if platform.is_windows?
+      rubygems_update_commands << "cp #{bundle_bins[ruby_api]} #{bundle_bins[ruby_api]}.bak" if platform.is_windows?
 
       # Update gem command on additional rubies to latest to avoid getting pre-release facter gems?
-      rubygems_version = rubyver =~ /^2\.1/ ? "2.7.8" : "" # PDK-1247 Pin ruby 2.1.9 to latest compatible rubygems.
-      build_commands << "#{gem_bins[local_settings[:ruby_api]]} update --system #{rubygems_version} --no-document"
+      rubygems_version = ruby_version =~ /^2\.1/ ? "2.7.9" : "" # PDK-1247 Pin ruby 2.1.9 to latest compatible rubygems.
+      rubygems_update_commands << "#{gem_bins[ruby_api]} update --system #{rubygems_version} --no-document"
 
       # ...replace the gem and bundler wrapper batch files file the backups we made.
-      build_commands << "mv #{gem_bins[local_settings[:ruby_api]]}.bak #{gem_bins[local_settings[:ruby_api]]}" if platform.is_windows?
-      build_commands << "mv #{bundle_bins[local_settings[:ruby_api]]}.bak #{bundle_bins[local_settings[:ruby_api]]}" if platform.is_windows?
+      rubygems_update_commands << "mv #{gem_bins[ruby_api]}.bak #{gem_bins[ruby_api]}" if platform.is_windows?
+      rubygems_update_commands << "mv #{bundle_bins[ruby_api]}.bak #{bundle_bins[ruby_api]}" if platform.is_windows?
+
+      rubygems_update_commands
     end
 
+    build_commands = []
+
+    # Update rubygems on "primary" Ruby
+    build_commands += rubygems_update.call(settings[:ruby_version], settings[:ruby_api])
+
+    # Update rubygems on "additional" rubies
+    settings[:additional_rubies]&.each do |rubyver, local_settings|
+      build_commands += rubygems_update.call(rubyver, local_settings[:ruby_api])
+    end
+
+    # Install "puppet" gem versions into appropriate Ruby installations.
     build_commands += puppet_rubyapi_versions.collect do |pupver, rubyapi|
       gem_install.call(rubyapi, 'puppet', pupver)
     end

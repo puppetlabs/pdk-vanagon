bb21a29eb8bb729bd4c7de139bc1374a9062c714
Jesse Scott <jesse@puppetlabs.com>
2020-04-10 10:46:47 -0700

    (MAINT) Bump Ruby version in PowerShell wrapper script

--- resources/files/windows/PuppetDevelopmentKit/PuppetDevelopmentKit.psm1
+++ resources/files/windows/PuppetDevelopmentKit/PuppetDevelopmentKit.psm1
@@ -3,7 +3,7 @@ $fso = New-Object -ComObject Scripting.FileSystemObject
 $script:DEVKIT_BASEDIR = (Get-ItemProperty -Path "HKLM:\Software\Puppet Labs\DevelopmentKit").RememberedInstallDir64
 # Windows API GetShortPathName requires inline C#, so use COM instead
 $script:DEVKIT_BASEDIR = $fso.GetFolder($script:DEVKIT_BASEDIR).ShortPath
-$script:RUBY_DIR       = "$($script:DEVKIT_BASEDIR)\private\ruby\2.4.9"
+$script:RUBY_DIR       = "$($script:DEVKIT_BASEDIR)\private\ruby\2.4.10"
 
 function pdk {
   if ($Host.Name -eq 'Windows PowerShell ISE Host') {

d1fe7d498a3582a5adfdc54e12ec5859597d0703
Jacklyn Kinsler <jacklyn.kinsler@puppet.com>
2021-03-31 15:08:56 -0700

    (VANAGON-85) Move platform definitions into core vanagon. Inherit definitions from a default platform.

--- configs/platforms/el-6-x86_64.rb
+++ configs/platforms/el-6-x86_64.rb
@@ -1,10 +1,3 @@
 platform "el-6-x86_64" do |plat|
-  plat.servicedir "/etc/rc.d/init.d"
-  plat.defaultdir "/etc/sysconfig"
-  plat.servicetype "sysv"
-  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/el/6/x86_64/pl-build-tools-release-6-1.noarch.rpm"
-  plat.provision_with "rpm --import http://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs"
-  plat.provision_with "yum install --assumeyes --nogpgcheck autoconf automake createrepo rsync gcc gcc-c++ make rpmdevtools rpm-libs yum-utils"
-  plat.install_build_dependencies_with "yum install --assumeyes"
-  plat.vmpooler_template "redhat-6-x86_64"
+  plat.inherit_from_default
 end

67e12dcdbe17db3df8788305947ec188b2da11f8
Dave Armstrong <david.armstrong@puppet.com>
2021-06-11 11:39:48 +0100

    (SEC-396) Native extension updates for racc

--- configs/components/rubygem-racc.rb
+++ configs/components/rubygem-racc.rb
@@ -6,7 +6,23 @@ component 'rubygem-racc' do |pkg, settings, platform|
   pkg.build_requires "pdk-runtime"
 
   if platform.is_windows?
-    pkg.environment 'PATH', settings[:gem_path_env]
+    ruby_gem_ver = 'ruby-2.4.0'
+    build_env_path = [
+      "$(shell cygpath -u C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/bin)",
+      "$(shell cygpath -u #{settings[:tools_root]}/bin)",
+      "$(shell cygpath -u #{settings[:tools_root]}/include)",
+      "$(shell cygpath -u #{settings[:bindir]})",
+      "$(shell cygpath -u #{settings[:ruby_bindir]})",
+      "$(shell cygpath -u #{settings[:includedir]})",
+      "/cygdrive/c/Windows/system32",
+      "/cygdrive/c/Windows",
+      "/cygdrive/c/Windows/System32/WindowsPowerShell/v1.0",
+      "$(PATH)",
+    ].join(':')
+
+    pkg.environment "PATH", build_env_path
+    pkg.environment 'CONFIGURE_ARGS', "--with-cflags='-I#{settings[:includedir]}/#{settings[ruby_gem_ver]}'"
+
   end
 
   pkg.install do

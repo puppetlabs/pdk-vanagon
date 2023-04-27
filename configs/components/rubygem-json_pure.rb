44ce12010c36be5c2425dfb2e7abab6f40093f56
Craig Gumbley <craiggumbley@gmail.com>
2023-04-27 17:12:19 +0100

    (CONT-935) Bump component versions

--- configs/components/rubygem-json_pure.rb
+++ configs/components/rubygem-json_pure.rb
@@ -1,6 +1,6 @@
 component "rubygem-json_pure" do |pkg, settings, platform|
-  pkg.version "2.6.2"
-  pkg.sha256sum 'ccf59aeb76249a17d894f0a974073d1264645528f0799a59c52b01560da3a811'
+  pkg.version "2.6.3"
+  pkg.sha256sum 'c39185aa41c04a1933b8d66d1294224743262ee6881adc7b5a488ab2ae19c74e'
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end

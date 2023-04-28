81286591aa95e972c43a85218f9e29b43a43420f
Craig Gumbley <craiggumbley@gmail.com>
2023-04-28 12:25:24 +0100

    (CONT-935) Remove migrated components

--- configs/components/rubygem-facter.rb
+++
@@ -1,6 +0,0 @@
-component "rubygem-facter" do |pkg, settings, platform|
-  pkg.version "4.3.0"
-  pkg.sha256sum "d24597d0fdc6a9219cb16f57f71512cd97219611775e24868bd30975b06cad2c"
-
-  instance_eval File.read('configs/components/_base-rubygem.rb')
-end
